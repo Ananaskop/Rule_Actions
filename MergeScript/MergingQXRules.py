38c0e5b8e0d7b4f1da4d2ddcb917a0c7b3ea9508
Gene Wong <gene@likesto.me>
2024-01-20 23:56:30 +0800

    Update MergingQXRules.py

--- MergeScript/MergingQXRules.py
+++ MergeScript/MergingQXRules.py
@@ -11,7 +11,7 @@ from concurrent.futures import ThreadPoolExecutor
 replacements = [
     (r'\s+', ''),
     (r',no-resolve', ''),
-    (r',(?:Netflix|Disney|YouTube|Spotify|AppleTV|Google)$', ',Streaming'),
+    (r',(?:Netflix|Disney|YouTube|Spotify|AppleTV|Google|TikTok)$', ',Streaming'),
     (r',(?:ChinaASN|China|Direct|Microsoft|Speedtest|Apple|Lan)$', ',direct'),
     (r',(?:AdvertisingLite|Privacy|Hijacking|BlockHttpDNS|REJECT|Reject)$', ',reject'),
     (r',(?:Proxy)$', ',proxy'),
@@ -54,6 +54,7 @@ RULES = {
         "Bing": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Bing/Bing.list",
     },
     "StreamingRule": {
+        "TikTok": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/TikTok/TikTok.list",
         "Netflix": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Netflix/Netflix.list",
         "Disney": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Disney/Disney.list",
         "YouTube": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/YouTube/YouTube.list",
