ea16cc94caeef9e8db662018e3f3382df7621c10
Gene Wong <gene@likesto.me>
2024-02-28 07:36:55 +0800

    更新 MergingSurgeRules.py

--- MergeScript/MergingSurgeRules.py
+++ MergeScript/MergingSurgeRules.py
@@ -47,7 +47,6 @@ RULES = {
         "PayPal": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/PayPal/PayPal.list",
         "Bing": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Bing/Bing.list",
         "Google": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Google/Google.list",
-        "AppleProxy": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/AppleProxy/AppleProxy.list",
         "OneDrive": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/OneDrive/OneDrive.list",
     },
     "StreamingRule": {
