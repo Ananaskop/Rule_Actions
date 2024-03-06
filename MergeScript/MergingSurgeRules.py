6b30eb7fb5b44c2109dd01f9fd8ec13bf5faf8d6
给你点32个赞 <gene@likesto.me>
2024-03-07 06:55:58 +0800

    更新 MergingSurgeRules.py

--- MergeScript/MergingSurgeRules.py
+++ MergeScript/MergingSurgeRules.py
@@ -28,7 +28,6 @@ RULES = {
         "Hijacking": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Hijacking/Hijacking.list",
         "MyBlockAds": "https://raw.githubusercontent.com/RuCu6/QuanX/main/Rules/MyBlockAds.list",
         "MyRejectRule": "https://raw.githubusercontent.com/GiveYou32Likes/Profile/main/QuantumultX/Rule/RejectRule.list",
-        "BlockHttpDNS": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BlockHttpDNS/BlockHttpDNS.list",
     },
     "ProxyRule": {
         "Proxy": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Proxy/Proxy_All.list",
