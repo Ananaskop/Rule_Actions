98e1604c76379c4a716fc7cb190c1450bc698ba8
Ananaskop <gene@likesto.me>
2024-07-09 22:45:08 +0800

    Update MergingQXRules.py

--- MergeScript/MergingQXRules.py
+++ MergeScript/MergingQXRules.py
@@ -26,7 +26,6 @@ replacements = [
 
 RULES = {
     "CorrectionRule": {
-        "Direct": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/QuantumultX/Direct/Direct.list",
         "MyCorrectionRule": "https://raw.githubusercontent.com/Ananaskop/Profile/main/QuantumultX/Rule/CorrectionRule.list",
     },
     "RejectRule": {
