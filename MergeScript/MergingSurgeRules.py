dd172889d0b0d3199b4f5e158859097c0add0176
Gene <123@gmail.com>
2024-09-26 14:11:00 +0800

    更新

--- MergeScript/MergingSurgeRules.py
+++ MergeScript/MergingSurgeRules.py
@@ -46,6 +46,7 @@ RULES = {
     "USRule": {
         "AI": "https://raw.githubusercontent.com/luestr/ProxyResource/refs/heads/main/Tool/Loon/Rule/AI.list",
         "PayPal": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/PayPal/PayPal.list",
+        "OneDrive": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/OneDrive/OneDrive.list",
     },
 }
 
