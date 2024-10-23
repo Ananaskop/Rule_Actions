ae99b9c3af758108b99e91e9174dfd286e82a1c3
Ananaskop <ananaskop@icloud.com>
2024-10-23 21:49:12 +0800

    更新 MergingRules.py

--- .github/MergingRules.py
+++ .github/MergingRules.py
@@ -26,7 +26,6 @@ RULES = {
     },
     "RejectRule": {
         "Hijacking": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/Hijacking/Hijacking.list",
-        "BlockHttpDNS": "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Surge/BlockHttpDNS/BlockHttpDNS.list",
         "AWAvenueAdsRule": "https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/Filters/AWAvenue-Ads-Rule-Surge-RULE-SET.list",
         "MyRejectRule": "https://raw.githubusercontent.com/Ananaskop/Profile/main/Rules/RejectRule.list",
     },
